e5b6b07c53c1125b877619a7115097426477cdfe
minjekim12 <101533438+minjekim12@users.noreply.github.com>
2022-06-22 11:54:05 +1200

    feat: added start button

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ to delete if they are moving on.
 # Import Tkinter and Submodules
 from tkinter import *
 from tkinter import ttk
+from tracemalloc import start
 
 
 # Program settings
@@ -33,12 +34,19 @@ root.geometry("800x600")
 
 
 #  ↳ Define START 
+def start():
+    NONE
+    # I left this command to do nothing for now. 
 
 #  ↳ Create label to display name of the program
 
+#  ↳ Define image for start button
+start_image = PhotoImage(file="Resources/start2.png")
+
 #  ↳ Create start button
-
-
+start_button = Button(root, image=start_image, command=start, height=95, width=225)
+start_button.pack(pady=220)
+    # TODO: Place the start button lower on the screen without cutting off any of the image
 
 
 # Create a ttk.Treeview widget 
